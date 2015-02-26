f5b9db4613bab0cdafd8aa8a1873857495e3b9f3
zeynep esen <zfm@anadolu.edu.tr>
2015-02-26 23:59:25 +0200

    Satir sutun kontrolleri düzeltildi

--- sudoku/Form1.cs
+++ sudoku/Form1.cs
@@ -16,8 +16,8 @@ namespace sudoku
         Random rnd = new Random();
         int uretilenSayi = 100;
         int[,] sudokuDizisi = new int[9, 9];
-        bool donenDeger;
-        bool deger, t;
+     
+        bool sayiUygunmu, t;
         public Form1()
         {
             InitializeComponent();
@@ -30,63 +30,71 @@ namespace sudoku
             {
                 for (int k = 0; k < 9; k++)
                 {
-                    while (deger == false)
+                    do
                     {
                         uretilenSayi = rnd.Next(1, 10);
-                        MessageBox.Show(uretilenSayi.ToString());
-                        SatirKontrol(i, k, uretilenSayi);
-                        t = SatirKontrol(i, k, uretilenSayi);
-                        MessageBox.Show(t.ToString() + " satir");
-                        t = SutunKontrol(i, k, uretilenSayi);
-                        MessageBox.Show(t.ToString() + " sütün");
-                        if (t == true)
+                        //MessageBox.Show(uretilenSayi.ToString());
+                        sayiUygunmu = SatirKontrol(i, k, uretilenSayi);
+                        if (!sayiUygunmu)
+                        {
+                            continue;
+                        }
+                       
+                        sayiUygunmu = SutunKontrol(i, k, uretilenSayi);
+                        if (!sayiUygunmu)
+                        {
+                            continue;
+                        }
+                       
+                        if (sayiUygunmu)
                         {
                             listBox1.Items.Add(uretilenSayi);
                             sudokuDizisi[i, k] = uretilenSayi;
-                            MessageBox.Show("diziye eklenen" + uretilenSayi.ToString());
+                            //MessageBox.Show("diziye eklenen" + uretilenSayi.ToString());
                         }
-                    }
+                    } while (!sayiUygunmu);
                 }
 
 
             }
+            MessageBox.Show("Bitti");
         }
 
         private bool SatirKontrol(int satir, int sutun, int kontrolEdileccekSayi)
         {
-            for (int i = 0; i < sutun; i++)
-            {
-                for (int k = 0; k < satir; k++)
+            bool donenDeger=true;
+                for (int k = 0; k <= sutun; k++)
                 {
-                    if (sudokuDizisi[i, k] != kontrolEdileccekSayi)
+                    if (sudokuDizisi[satir, k] == kontrolEdileccekSayi)
                     {
-                        donenDeger = true;
+                        donenDeger = false;
 
                     }
 
                 }
-
-            }
             return donenDeger;
 
         }
         private bool SutunKontrol(int satir, int sutun, int kontrolEdileccekSayi)
         {
-            for (int i = 0; i < satir; i++)
-            {
-                for (int k = 0; k < sutun; k++)
+            bool donenDeger=true;
+                for (int k = 0; k <= satir; k++)
                 {
-                    if (sudokuDizisi[i, k] != kontrolEdileccekSayi)
+                    if (sudokuDizisi[k, sutun] == kontrolEdileccekSayi)
                     {
                         donenDeger = true;
 
                     }
 
 
-                }
-            }
+                }        
 
             return donenDeger;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
